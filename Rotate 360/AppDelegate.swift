135a4f7be62deb3aab36c57fc25df1e679d679a8
Taemin Yun <dbsxoals689@iCloud.com>
2020-06-16 18:54:02 +0900

    Initial Commit

---
+++ Rotate 360/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Rotate 360
+//
+//  Created by 윤태민 on 6/16/20.
+//  Copyright © 2020 Taemin Yun. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Rotate 360/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Rotate 360
+//
+//  Created by 윤태민 on 6/16/20.
+//  Copyright © 2020 Taemin Yun. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
